f471f093216925b3c92886a874d9b713fe9af547
fyww <79212727+fywws@users.noreply.github.com>
2023-12-08 19:50:52 +0300

    fixes
    add include dir right into solution

--- testh/testh.cpp
+++ testh/testh.cpp
@@ -4,18 +4,22 @@
 
 #include "SFML/Graphics.hpp"
 #include <iostream>
-
+#include <string>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode::getFullscreenModes()[0], "Window Title", sf::Style::Fullscreen);
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Window Title", sf::Style::Default);
     ImGui::SFML::Init(window);
 
     float radius = 10;
     float velocity = 5;
+    ImColor color(255, 255, 255);
+    ImColor bgColor(148, 100, 130);
 
-    sf::CircleShape shape(radius, 40);
-    shape.setPosition(960, 540);
+
+    sf::CircleShape* shape = new sf::CircleShape(radius, 40);
+    shape->setPosition(960, 540);
+    shape->setFillColor(sf::Color(color));
 
     bool isDragging = false;  // Flag to track if the shape is being dragged
     sf::Vector2f offset;      // Offset to move the shape based on cursor position
@@ -37,16 +41,19 @@ int main()
             if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left)
             {
                 sf::Vector2f mousePos = static_cast<sf::Vector2f>(sf::Mouse::getPosition(window));
-                if (shape.getGlobalBounds().contains(mousePos))
+                if (shape->getGlobalBounds().contains(mousePos))
                 {
                     isDragging = true;
-                    offset = shape.getPosition() - mousePos;
+                    offset = shape->getPosition() - mousePos;
                 }
             }
 
             if (event.type == sf::Event::MouseButtonReleased && event.mouseButton.button == sf::Mouse::Left)
             {
                 isDragging = false;
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Delete))
+            {
             }
         }
 
@@ -56,20 +63,25 @@ int main()
         if (isDragging)
         {
             sf::Vector2f mousePos = static_cast<sf::Vector2f>(sf::Mouse::getPosition(window));
-            shape.setPosition(mousePos + offset);
+            shape->setPosition(mousePos + offset);
         }
 
 
-        ImGui::Begin("Window title");
-        ImGui::Text("Window text!");
-        ImGui::SliderFloat("Test", &radius, 1.f, 300.f);
+        ImGui::Begin("Shape edit");
+        ImGui::SliderFloat("Radius", &radius, 1.f, 300.f);
+        ImGui::ColorEdit4("Color", (float*)&color);
         // Color picker for the shape
         ImGui::End();
 
-        shape.setRadius(radius);
+        ImGui::Begin("Render");
+        ImGui::ColorEdit4("Color", (float*)&bgColor);
 
-        window.clear(sf::Color(18, 33, 43));
-        window.draw(shape);
+        ImGui::End();
+
+        shape->setRadius(radius);
+        shape->setFillColor(sf::Color(color));
+        window.clear(sf::Color(bgColor));
+        window.draw(*shape);
         ImGui::SFML::Render(window);
 
         window.display();
